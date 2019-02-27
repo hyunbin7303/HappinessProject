1a028cd43e3fdbc3a733f69ddd5ae650a5a56512
jrivas6219 <jrivas6219@conestogac.on.ca>
2019-02-27 16:02:45 -0500

    changed icons

--- HP/Pizzaria1-master/Pizzaria1/ScoreTracker.Designer.cs
+++ HP/Pizzaria1-master/Pizzaria1/ScoreTracker.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cb_StartOnTime = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cb_FinishOnTime = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_AchievementLevel = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_Workload = new System.Windows.Forms.ComboBox();
+            this.lab_Achievement = new System.Windows.Forms.Label();
+            this.cb_Concentration = new System.Windows.Forms.ComboBox();
+            this.pb_Overall = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.btn_Cancel = new System.Windows.Forms.Button();
             this.btn_Youtube = new System.Windows.Forms.Button();
             this.btn_Save = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pb_Overall = new System.Windows.Forms.ProgressBar();
-            this.cb_Concentration = new System.Windows.Forms.ComboBox();
-            this.lab_Achievement = new System.Windows.Forms.Label();
-            this.cb_Workload = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.cb_AchievementLevel = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cb_FinishOnTime = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.cb_StartOnTime = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,105 +59,59 @@
             this.groupBox1.Controls.Add(this.cb_Concentration);
             this.groupBox1.Controls.Add(this.pb_Overall);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(330, 389);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(248, 316);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // btn_Cancel
+            // cb_StartOnTime
             // 
-            this.btn_Cancel.Location = new System.Drawing.Point(249, 416);
-            this.btn_Cancel.Name = "btn_Cancel";
-            this.btn_Cancel.Size = new System.Drawing.Size(94, 39);
-            this.btn_Cancel.TabIndex = 1;
-            this.btn_Cancel.Text = "Cancel";
-            this.btn_Cancel.UseVisualStyleBackColor = true;
-            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            this.cb_StartOnTime.FormattingEnabled = true;
+            this.cb_StartOnTime.Items.AddRange(new object[] {
+            "",
+            "Yes",
+            "No"});
+            this.cb_StartOnTime.Location = new System.Drawing.Point(116, 52);
+            this.cb_StartOnTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cb_StartOnTime.Name = "cb_StartOnTime";
+            this.cb_StartOnTime.Size = new System.Drawing.Size(108, 21);
+            this.cb_StartOnTime.TabIndex = 13;
             // 
-            // btn_Youtube
+            // label4
             // 
-            this.btn_Youtube.Location = new System.Drawing.Point(12, 364);
-            this.btn_Youtube.Name = "btn_Youtube";
-            this.btn_Youtube.Size = new System.Drawing.Size(0, 0);
-            this.btn_Youtube.TabIndex = 1;
-            this.btn_Youtube.Text = "Put your video";
-            this.btn_Youtube.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 52);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Start On Time";
             // 
-            // btn_Save
+            // cb_FinishOnTime
             // 
-            this.btn_Save.Location = new System.Drawing.Point(148, 416);
-            this.btn_Save.Name = "btn_Save";
-            this.btn_Save.Size = new System.Drawing.Size(95, 39);
-            this.btn_Save.TabIndex = 2;
-            this.btn_Save.Text = "Save";
-            this.btn_Save.UseVisualStyleBackColor = true;
-            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            this.cb_FinishOnTime.FormattingEnabled = true;
+            this.cb_FinishOnTime.Items.AddRange(new object[] {
+            "",
+            "Yes",
+            "No"});
+            this.cb_FinishOnTime.Location = new System.Drawing.Point(116, 224);
+            this.cb_FinishOnTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cb_FinishOnTime.Name = "cb_FinishOnTime";
+            this.cb_FinishOnTime.Size = new System.Drawing.Size(108, 21);
+            this.cb_FinishOnTime.TabIndex = 11;
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Concentration";
-            // 
-            // pb_Overall
-            // 
-            this.pb_Overall.Location = new System.Drawing.Point(16, 316);
-            this.pb_Overall.Name = "pb_Overall";
-            this.pb_Overall.Size = new System.Drawing.Size(281, 23);
-            this.pb_Overall.TabIndex = 4;
-            this.pb_Overall.Click += new System.EventHandler(this.pb_Overall_Click);
-            // 
-            // cb_Concentration
-            // 
-            this.cb_Concentration.FormattingEnabled = true;
-            this.cb_Concentration.Items.AddRange(new object[] {
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.cb_Concentration.Location = new System.Drawing.Point(154, 115);
-            this.cb_Concentration.Name = "cb_Concentration";
-            this.cb_Concentration.Size = new System.Drawing.Size(143, 24);
-            this.cb_Concentration.TabIndex = 5;
-            // 
-            // lab_Achievement
-            // 
-            this.lab_Achievement.AutoSize = true;
-            this.lab_Achievement.Location = new System.Drawing.Point(28, 225);
-            this.lab_Achievement.Name = "lab_Achievement";
-            this.lab_Achievement.Size = new System.Drawing.Size(127, 17);
-            this.lab_Achievement.TabIndex = 6;
-            this.lab_Achievement.Text = "Satisfaction";
-            // 
-            // cb_Workload
-            // 
-            this.cb_Workload.FormattingEnabled = true;
-            this.cb_Workload.Items.AddRange(new object[] {
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.cb_Workload.Location = new System.Drawing.Point(154, 170);
-            this.cb_Workload.Name = "cb_Workload";
-            this.cb_Workload.Size = new System.Drawing.Size(143, 24);
-            this.cb_Workload.TabIndex = 7;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 170);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 17);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Level of Completion";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 227);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Finish On Time";
             // 
             // cb_AchievementLevel
             // 
@@ -169,63 +123,126 @@
             "3",
             "4",
             "5"});
-            this.cb_AchievementLevel.Location = new System.Drawing.Point(154, 225);
+            this.cb_AchievementLevel.Location = new System.Drawing.Point(116, 183);
+            this.cb_AchievementLevel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cb_AchievementLevel.Name = "cb_AchievementLevel";
-            this.cb_AchievementLevel.Size = new System.Drawing.Size(143, 24);
+            this.cb_AchievementLevel.Size = new System.Drawing.Size(108, 21);
             this.cb_AchievementLevel.TabIndex = 9;
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 279);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 17);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Finish On Time";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 141);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Level of Completion";
             // 
-            // cb_FinishOnTime
+            // cb_Workload
             // 
-            this.cb_FinishOnTime.FormattingEnabled = true;
-            this.cb_FinishOnTime.Items.AddRange(new object[] {
-            "",
-            "Yes",
-            "No"});
-            this.cb_FinishOnTime.Location = new System.Drawing.Point(154, 276);
-            this.cb_FinishOnTime.Name = "cb_FinishOnTime";
-            this.cb_FinishOnTime.Size = new System.Drawing.Size(143, 24);
-            this.cb_FinishOnTime.TabIndex = 11;
+            this.cb_Workload.FormattingEnabled = true;
+            this.cb_Workload.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cb_Workload.Location = new System.Drawing.Point(116, 138);
+            this.cb_Workload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cb_Workload.Name = "cb_Workload";
+            this.cb_Workload.Size = new System.Drawing.Size(108, 21);
+            this.cb_Workload.TabIndex = 7;
             // 
-            // label4
+            // lab_Achievement
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 64);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 17);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "Start On Time";
+            this.lab_Achievement.AutoSize = true;
+            this.lab_Achievement.Location = new System.Drawing.Point(21, 183);
+            this.lab_Achievement.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_Achievement.Name = "lab_Achievement";
+            this.lab_Achievement.Size = new System.Drawing.Size(62, 13);
+            this.lab_Achievement.TabIndex = 6;
+            this.lab_Achievement.Text = "Satisfaction";
             // 
-            // cb_StartOnTime
+            // cb_Concentration
             // 
-            this.cb_StartOnTime.FormattingEnabled = true;
-            this.cb_StartOnTime.Items.AddRange(new object[] {
-            "",
-            "Yes",
-            "No"});
-            this.cb_StartOnTime.Location = new System.Drawing.Point(154, 64);
-            this.cb_StartOnTime.Name = "cb_StartOnTime";
-            this.cb_StartOnTime.Size = new System.Drawing.Size(143, 24);
-            this.cb_StartOnTime.TabIndex = 13;
+            this.cb_Concentration.FormattingEnabled = true;
+            this.cb_Concentration.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cb_Concentration.Location = new System.Drawing.Point(116, 93);
+            this.cb_Concentration.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cb_Concentration.Name = "cb_Concentration";
+            this.cb_Concentration.Size = new System.Drawing.Size(108, 21);
+            this.cb_Concentration.TabIndex = 5;
             // 
-            // Breakdance
+            // pb_Overall
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.pb_Overall.Location = new System.Drawing.Point(12, 257);
+            this.pb_Overall.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pb_Overall.Name = "pb_Overall";
+            this.pb_Overall.Size = new System.Drawing.Size(211, 19);
+            this.pb_Overall.TabIndex = 4;
+            this.pb_Overall.Click += new System.EventHandler(this.pb_Overall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Concentration";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(187, 338);
+            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(70, 32);
+            this.btn_Cancel.TabIndex = 1;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // btn_Youtube
+            // 
+            this.btn_Youtube.Location = new System.Drawing.Point(9, 296);
+            this.btn_Youtube.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Youtube.Name = "btn_Youtube";
+            this.btn_Youtube.Size = new System.Drawing.Size(0, 0);
+            this.btn_Youtube.TabIndex = 1;
+            this.btn_Youtube.Text = "Put your video";
+            this.btn_Youtube.UseVisualStyleBackColor = true;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(111, 338);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(71, 32);
+            this.btn_Save.TabIndex = 2;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // ScoreTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 467);
+            this.ClientSize = new System.Drawing.Size(270, 379);
             this.Controls.Add(this.btn_Save);
             this.Controls.Add(this.btn_Youtube);
             this.Controls.Add(this.btn_Cancel);
             this.Controls.Add(this.groupBox1);
-            this.Name = "Breakdance";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "ScoreTracker";
             this.Text = "Breakdance";
             this.Load += new System.EventHandler(this.Breakdance_Load);
             this.groupBox1.ResumeLayout(false);
